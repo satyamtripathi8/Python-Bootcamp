a899d801834cdd1457b75e486dd2eea3d892dc60
satyamtripathi8 <satyamtripathi038@gmail.com>
2023-04-25 18:55:55 +0530

    Day 16

---
+++ Day16/check_power_2.py
@@ -0,0 +1,19 @@
+x=int(input())
+# if x==0:
+#     print("Not possible")
+# else:    
+#     while(x%2==0):
+#         x=x//2
+# if(x==1):
+#     print("true")
+# else:
+#     print("False")
+
+# Using Recursion
+def check(n):
+    if n==1:
+        return True
+    if n==0 or n%2!=0:
+        return False
+    return check(n//2)
+print(check(x))
