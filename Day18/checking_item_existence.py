5e97b284ad04099431474faa18d62041052daecf
satyamtripathi8 <satyamtripathi038@gmail.com>
2023-06-26 23:24:55 +0530

    OS

--- Day18/checking_item_existence.py
+++ Day18/checking_item_existence.py
@@ -1,8 +1,7 @@
 import os
 from os import path
-import datetime
-from datetime import date , time , timedelta
-import time 
+
+
 
 x = input("Enter file name with its extension: ")
 if path.isfile(x):
--- Day18/checking_item_type.py
+++ Day18/checking_item_type.py
@@ -1,10 +1,8 @@
 import os
 from os import path
-import datetime
-from datetime import date , time , timedelta
-import time 
 
-x = input("Enter existing file name with its extension: ")
+
+x = input("Enter file name with its extension: ")
 if path.isfile(x):
     print("It is File")
 elif path.isdir(x):
